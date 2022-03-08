1a84ec3bde0dee23f71a21255c7b28cf4f72722c
Nurul Islam <nurulbdpersonal@gmail.com>
2022-03-08 08:50:45 +0600

    pr added

---
+++ pr.js
@@ -0,0 +1,8 @@
+let str = ""; 
+console.log(str);
+
+const marks= 60;
+const resubmit = -5; 
+console.log(!(marks > 0 || resubmit > 0));
+
+const person = {name: "hero", id: 101, address: "BD",postalCode: 40321};
