356429264f1b8ae12d3a0ce1b21e0028f67e9042
Nurul Islam <nurulbdpersonal@gmail.com>
2022-03-09 16:54:36 +0600

    undefined added

--- pr.js
+++ pr.js
@@ -1,3 +1,4 @@
+/*
 let str = ""; 
 console.log(str);
 
@@ -6,3 +7,9 @@ const resubmit = -5;
 console.log(!(marks > 0 || resubmit > 0));
 
 const person = {name: "hero", id: 101, address: "BD",postalCode: 40321};
+*/
+
+const x = 5;
+const y = 3;
+const reason = 'x is expected to be less than y';
+console.assert(x < y, {x, y, reason});
